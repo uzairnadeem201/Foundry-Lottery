4aaad742c7b2e83f294e36e531542638e28f375e
uzairnadeem201 <uzairnadeem201@gmail.com>
2025-01-21 07:03:30 +0500

    Enter Raffle Function done

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,24 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
 //SPDX-License_Identifier: MIT
 pragma solidity 0.8.19;
 
@@ -8,15 +29,33 @@ pragma solidity 0.8.19;
  * @dev Implements Chainlink VRFv2.5
  */
 contract Raffle {
+    /** Errors */
+    error Raffle_SendMoreToEnterRaffle();
     uint256 private immutable i_enteranceFee;
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
+    address payable[] private s_players;
+    /* Events */
+    event RaffleEntered(address indexed player);
 
-    constructor(uint256 enteranceFee) {
+    constructor(uint256 enteranceFee, uint256 interval) {
         i_enteranceFee = enteranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() external payable {
+        //require(msg.value >= i_enteranceFee, "Not Enough ETH Sent");
+        if (msg.value < i_enteranceFee) {
+            revert Raffle_SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
+    }
 
-    function pickWinner() public {}
+    function pickWinner() external {
+        if ((block.timestamp - s_lastTimeStamp) > i_interval) {}
+    }
 
     /** Getters */
     function getEnteranceFee() external returns (uint256) {
